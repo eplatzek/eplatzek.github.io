ba4e7bebca906e7a2b5d8521b38db4dcf992fcbb
ericplatzek <ericplatzek@gmail.com>
2014-10-01 10:56:35 -0700

    Classs from 9/30 and Edits to Resume Site

---
+++ resume-demo/php/header.php
@@ -0,0 +1,7 @@
+<header>
+        <div class="container">
+            <img src="img/mac.jpg" alt="Mac Computer">
+            <img src="img/aperture.png" alt="Photography">
+            <img src="img/pc.jpg" alt="PC">
+        </div>
+</header>
