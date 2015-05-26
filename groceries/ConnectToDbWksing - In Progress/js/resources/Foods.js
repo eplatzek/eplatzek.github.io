8a70b3e19954a926e4fdd05788cf53ebfb9de305
eplatzek <ericplatzek@gmail.com>
2015-05-25 22:50:58 -0700

    Edited Groceries

--- groceries/ConnectToDbWksing - In Progress/js/resources/Foods.js
+++ groceries/ConnectToDbWksing - In Progress/js/resources/Foods.js
@@ -1,5 +1,5 @@
 FoodModule.factory('Foods', function ($resource) {
-console.log("FF");
+    console.log("FF");
                   
     var Foods = $resource('https://api.mongolab.com/api/1/databases/swagdb/collections/fda/:id', {
         apiKey:'BjlEUWGYCJqtGRf2OpBLLXtZ_U-BFEmg',
