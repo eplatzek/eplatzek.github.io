f5801511b1fa76d2cd650721cf96d2a66ce0f486
eplatzek <ericplatzek@gmail.com>
2015-07-05 22:03:49 -0700

    Updated Days 1-4.
    Updated Days 1-4 to reflect actual class time. Changed NavBar to JS.
    Revised Remaining Days.

---
+++ js2/js/navbar.js
@@ -0,0 +1,30 @@
+document.addEventListener('DOMContentLoaded', function() {
+document.getElementById("myJS2Nav").innerHTML =' \
+ <div class="container-fluid">       \
+<div class="container-fluid">       \
+      <div class="navbar-header">    \
+        <a class="navbar-brand" href="#">JavaScript 2</a>    \
+      </div>    \
+      <div>    \
+        <ul class="nav navbar-nav">    \
+          <li class="dropdown">    \
+            <a class="dropdown-toggle active" data-toggle="dropdown" href="#">Days     \
+              <span class="caret"></span></a>    \
+              <ul class="dropdown-menu">    \
+                <li><a href="index.html">Day 1: Review</a></li>    \
+                <li><a href="day2.html">Day 2: Arrays</a></li>    \
+                <li><a href="day3.html">Day 3: Review of Array Challenges</a></li>    \
+                <li><a href="day3.html">Day 4: Objects</a></li>    \
+                <li><a href="day4.html">Day 4: Mongo DB</a></li>    \
+                <li><a href="day5.html">Day 5: Angular!!</a></li>    \
+                <li><a href="day6.html">Day 6: Mid Terms (Coding Challenge)</a></li>    \
+                <li><a href="day7.html">Day 7: Mid Terms (Presenting)</a></li>    \
+                <li><a href="day8.html">Day 8: Routing</a></li>    \
+                <li><a href="day9.html">Day 9,10,11,12: TBA</a></li>    \
+              </ul>    \
+            </li>    \
+          </ul>    \
+        </div>    \
+    </div>    \
+';                
+},false);
