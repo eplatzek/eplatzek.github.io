36f47633115b86eefe4db490bb364449c2e3b5ac
eplatzek <ericplatzek@gmail.com>
2015-07-06 22:17:10 -0700

    Update Navbar

--- js2/js/navbar.js
+++ js2/js/navbar.js
@@ -14,13 +14,13 @@ document.getElementById("myJS2Nav").innerHTML =' \
                 <li><a href="index.html">Day 1: Review</a></li>    \
                 <li><a href="day2.html">Day 2: Arrays</a></li>    \
                 <li><a href="day3.html">Day 3: Review of Array Challenges</a></li>    \
-                <li><a href="day3.html">Day 4: Objects</a></li>    \
-                <li><a href="day4.html">Day 4: Mongo DB</a></li>    \
-                <li><a href="day5.html">Day 5: Angular!!</a></li>    \
-                <li><a href="day6.html">Day 6: Mid Terms (Coding Challenge)</a></li>    \
-                <li><a href="day7.html">Day 7: Mid Terms (Presenting)</a></li>    \
-                <li><a href="day8.html">Day 8: Routing</a></li>    \
-                <li><a href="day9.html">Day 9,10,11,12: TBA</a></li>    \
+                <li><a href="day4.html">Day 4: Objects</a></li>    \
+                <li><a href="day5.html">Day 5: Mongo DB</a></li>    \
+                <li><a href="day6.html">Day 6: Angular!!</a></li>    \
+                <li><a href="day7.html">Day 7: Mid Terms (Coding Challenge)</a></li>    \
+                <li><a href="day8.html">Day 8: Mid Terms (Presenting)</a></li>    \
+                <li><a href="day9.html">Day 9: Routing</a></li>    \
+                <li><a href="day10.html">Day 10,11,12: TBA</a></li>    \
               </ul>    \
             </li>    \
           </ul>    \
