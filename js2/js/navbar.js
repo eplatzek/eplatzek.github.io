bde958fdc25e82938d7de8d305130d6de5379d62
eplatzek <ericplatzek@gmail.com>
2015-07-20 22:19:38 -0700

    JS2 - Updated Days 10-12

--- js2/js/navbar.js
+++ js2/js/navbar.js
@@ -17,9 +17,12 @@ document.getElementById("myJS2Nav").innerHTML =' \
                 <li><a href="day4.html">Day 4: Objects</a></li>    \
                 <li><a href="day5.html">Day 5: Mongo DB</a></li>    \
                 <li><a href="day6.html">Day 6: Mongo DB Continued</a></li>    \
-                <li><a href="day7.html">Day 7: Angular!!</a></li>    \
+                <li><a href="day7.html">Day 7: Angular Day 1!!</a></li>    \
                 <li><a href="day8.html">Day 8: Angular Day 2!!</a></li>    \
                 <li><a href="day9.html">Day 9: Error Checking</a></li>    \
+                <li><a href="day10.html">Day 10: Angular Review Day 3!!</a></li>    \
+                <li><a href="day11.html">Day 11 & 12: Final Project Review and Presentation</a></li>    \
+                <li><a href="routingDemo.html">Routing Demo</a></li>    \
               </ul>    \
             </li>    \
           </ul>    \
