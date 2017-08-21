8a3f7e0b2c8cfebd7c57f9722bb14c2a74eb4cd7
eplatzek <eplatzek@onfarm.com>
2017-08-20 23:24:39 -0700

    Coverage

--- war/spec/PlayerSpec.js
+++ war/spec/PlayerSpec.js
@@ -4,7 +4,7 @@ describe("Game", function() {
 
   it("should create a deck", function() {
     // WHEN
-    deck = exports.createDeck();
+    deck = createDeck();
 
     //THEN
     expect(deck).toBeDefined();
@@ -13,14 +13,60 @@ describe("Game", function() {
 
   it("should shuffle a deck", function() {
     // GIVEN
-    deck = exports.createDeck();
+    deck = createDeck();
     spyOn(Math,['random']);
 
     // WHEN
-    exports.shuffleDeck(deck);
+    shuffleDeck(deck);
 
     //THEN
+    expect(Math.Random).toBeHaveBeenCalled();
     expect(deck).toBeDefined();
     expect(deck.length).toBe(52);
   });
+
+  describe("should split a deck", function() {
+    it("two ways", function() {
+      // GIVEN
+      deck = createDeck();
+      players = [new Player('1'), new Player('2')];
+
+      // WHEN
+      splitDeck(deck, players);
+
+      //THEN
+      expect(players[0].currentDeck.length).toBe(26);
+      expect(players[1].currentDeck.length).toBe(26);
+    });
+
+    it("three ways", function() {
+      // GIVEN
+      deck = createDeck();
+      players = [new Player('1'), new Player('2'),  new Player('3')];
+
+      // WHEN
+      splitDeck(deck, players);
+
+      //THEN
+      expect(players[0].currentDeck.length).toBe(17);
+    });
+  });
+
+  describe("should convert deck values", function() {
+    it("of an ace", function() {
+      //THEN
+      expect(cardValueConversion('A')).toBe(11);
+    });
+
+    it("of a facecard", function() {
+      //THEN
+      expect(cardValueConversion('K')).toBe(10);
+    });
+
+    it("of a number", function() {
+      //THEN
+      expect(cardValueConversion('5')).toBe(5);
+    });
+  });
+
 });
